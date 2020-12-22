ffd328a1376e946f49cb2728b500c00ae5da6edf
Hajin Choi <hchoi405@gmail.com>
2020-12-22 18:38:23 +0900

    Change the directory

--- copy_textures.sh
+++ copy_textures.sh
@@ -1,3 +1,3 @@
-find -iname '*.jpg' -exec ./saveCopy {} ./scenes/textures \;
-find -iname '*.png' -exec ./saveCopy {} ./scenes/textures \;
-find -iname '*.bmp' -exec ./saveCopy {} ./scenes/textures \;
+find ./scenes/ -iname '*.jpg' -exec ./saveCopy {} ./textures \;
+find ./scenes/ -iname '*.png' -exec ./saveCopy {} ./textures \;
+find ./scenes/ -iname '*.bmp' -exec ./saveCopy {} ./textures \;
